d0d27770ac4743a5ef89b6bf08f661f9e7a6929d
Simone de santis <simone.agentelazio@yahoo.it>
2021-07-28 14:24:15 +0200

    inizio esercitazione

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/*
+Ciao ragazzi,
+Esercizio di oggi: Vue Slider
+nome repo: vue-slider
+Descrizione:
+Rifare l'esercizio dello slider come fatto assieme in classe.
+Bonus:
+Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente.
+IMPORTANTE: Non perdete troppo tempo sui dettagli grafici, l'importante sono le funzionalità , non riterremo  errori se la grafica non rispecchia in maniera identica quello che abbiamo fatto in classe
+*/
+
+console.log("js ok");
+
+Vue.config.devtools = true;
+
+const app = new Vue({
+  el: "#app",
+});
