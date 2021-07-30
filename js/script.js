313acb4400fa4f75db11a12f6946b2ae0717b92a
Simone de santis <simone.agentelazio@yahoo.it>
2021-07-30 14:27:36 +0200

    fine inserimento slide

--- js/script.js
+++ js/script.js
@@ -15,4 +15,31 @@ Vue.config.devtools = true;
 
 const app = new Vue({
   el: "#app",
+  data: {
+    currentIndex: 0,
+    linkImage: [
+      "./img/image1.jpg",
+      "./img/image2.jpg",
+      "./img/image3.jpg",
+      "./img/image4.jpg",
+    ],
+  },
+  methods: {
+    isActive(index) {
+      return this.currentIndex === index ? "active" : "";
+    },
+    increaseIndex() {
+      if (
+        this.currentIndex === this.linkImage.length - 1 ||
+        this.currentIndex > this.linkImage.length - 1
+      ) {
+        this.currentIndex = 0;
+      } else this.currentIndex++;
+    },
+    decreaseIndex() {
+      if (this.currentIndex === 0) {
+        this.currentIndex = 0;
+      } else this.currentIndex--;
+    },
+  },
 });
